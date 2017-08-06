d61c5409463ab25aabaa4130082b7bffe694bd4b
Tanu Priya <priyasinha396@gmail.com>
2017-08-06 15:17:58 -0700

    updated app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: '213c2245-3cb9-49a4-b852-41e4f6eea0b0', appPassword: '7WutpJBKjsXNAGALGVRMMiS' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
